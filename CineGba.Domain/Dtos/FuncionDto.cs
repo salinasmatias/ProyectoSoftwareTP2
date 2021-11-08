cccec76821ae530550131ce01ebb2857f64e8285
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-08 02:34:01 -0300

    modified funcion dto

--- CineGba.Domain/Dtos/FuncionDto.cs
+++ CineGba.Domain/Dtos/FuncionDto.cs
@@ -3,6 +3,7 @@
     public class FuncionDto
     {
         public int PeliculaId { get; set; }
+        public int FuncionId { get; set; }
         public int SalaId { get; set; }
         public string Fecha { get; set; }
         public string Horario { get; set;}
